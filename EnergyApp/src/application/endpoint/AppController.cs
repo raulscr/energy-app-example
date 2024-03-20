b941c66ba0d2fea1c1d22b8cfa6a89f133b7d9c2
Raul Scarmocin <raulsfreitas1997@hotmail.com>
2024-03-20 20:00:36 -0300

    Create Basic application controller with exit option only

---
+++ EnergyApp/src/application/endpoint/AppController.cs
@@ -0,0 +1,47 @@
+
+namespace Application
+{
+    public class AppController
+    {
+        private AppObserver Observer;
+
+        public bool Exit { get; private set; }
+
+        public string ProcessOption(string option)
+        {
+            switch (option)
+            {
+                case "1":
+                    // TODO: implement option 1
+                    return "Option 1 selected";
+                case "2":
+                    // TODO: implement option 2
+                    return "Option 2 selected";
+                case "3":
+                    // TODO: implement option 3
+                    return "Option 3 selected";
+                case "4":
+                    // TODO: implement option 4
+                    return "Option 4 selected";
+                case "5":
+                    // TODO: implement option 5
+                    return "Option 5 selected";
+                case "6":
+                    if (Observer.ConfirmExit())
+                    {
+                        Exit = true;
+                        return "Exiting the application...";
+                    }
+                    return "";
+                default:
+                    return "Invalid Option";
+            }
+        }
+
+        public AppController(AppObserver observer)
+        {
+            Observer = observer;
+            Exit = false;
+        }
+    }
+}
