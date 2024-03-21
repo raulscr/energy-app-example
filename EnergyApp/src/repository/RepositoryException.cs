30c2e12d598495508ea28559e65f71a94401eafb
Raul Scarmocin <raulsfreitas1997@hotmail.com>
2024-03-20 22:24:49 -0300

    create generic repository exception

---
+++ EnergyApp/src/repository/RepositoryException.cs
@@ -0,0 +1,17 @@
+
+public class RepositoryException : Exception
+{
+    public RepositoryException()
+    {
+    }
+
+    public RepositoryException(string message)
+        : base(message)
+    {
+    }
+
+    public RepositoryException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+}
