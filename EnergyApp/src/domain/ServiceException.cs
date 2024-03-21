04b06d2128ae68546d195a2a02641506b44213dc
Raul Scarmocin <raulsfreitas1997@hotmail.com>
2024-03-20 22:26:56 -0300

    create generic service exception (for business rules errors)

---
+++ EnergyApp/src/domain/ServiceException.cs
@@ -0,0 +1,16 @@
+public class ServiceException : Exception
+{
+    public ServiceException()
+    {
+    }
+
+    public ServiceException(string message)
+        : base(message)
+    {
+    }
+
+    public ServiceException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+}
